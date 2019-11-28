000e225ae2761aff4a0bc71549ca1dc9b905587b
Sergio Lo Porto <sergioloporto@gmail.com>
2019-11-28 09:37:35 +0100

    Updates

---
+++ precache-manifest.49d934186154b68d27a0b8c465897795.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0711867d794d0ddf8171fe6e99feaebd",
+    "url": "/Virus-or-bacteria-detector-REACT/index.html"
+  },
+  {
+    "revision": "4196f9a6b2d07a14e737",
+    "url": "/Virus-or-bacteria-detector-REACT/static/css/main.7c955913.chunk.css"
+  },
+  {
+    "revision": "0f124a1e284e44dbb473",
+    "url": "/Virus-or-bacteria-detector-REACT/static/js/2.d4e8691a.chunk.js"
+  },
+  {
+    "revision": "4196f9a6b2d07a14e737",
+    "url": "/Virus-or-bacteria-detector-REACT/static/js/main.c0b97456.chunk.js"
+  },
+  {
+    "revision": "61241c718b38ad393c15",
+    "url": "/Virus-or-bacteria-detector-REACT/static/js/runtime-main.57768d70.js"
+  },
+  {
+    "revision": "18da87610bbad3786f8bcd433233a3d8",
+    "url": "/Virus-or-bacteria-detector-REACT/static/media/fontello.18da8761.svg"
+  },
+  {
+    "revision": "944191a628a43f17acd881b3dbd1a852",
+    "url": "/Virus-or-bacteria-detector-REACT/static/media/fontello.944191a6.ttf"
+  },
+  {
+    "revision": "c9d5bc409a3c75a0c1fc2edb3318b33b",
+    "url": "/Virus-or-bacteria-detector-REACT/static/media/fontello.c9d5bc40.eot"
+  },
+  {
+    "revision": "edb5faeb5244f00bd0c0e7845210e110",
+    "url": "/Virus-or-bacteria-detector-REACT/static/media/fontello.edb5faeb.woff"
+  },
+  {
+    "revision": "f2089db893611ecb3981ae955accc02f",
+    "url": "/Virus-or-bacteria-detector-REACT/static/media/fontello.f2089db8.woff2"
+  }
+]);
